532bbe9bb8c382ca03c37f0d9912342a2e101592
Viktarovich Natallia <nata17v@mail.ru>
2025-07-13 12:02:58 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -9506,7 +9506,7 @@ function setupDesktopAnimations() {
     scrollTrigger: {
       trigger: ".animation__wrapper",
       start: "top 0%",
-      scrub: 1.5,
+      scrub: 1,
       end: "+=1700",
       anticipatePin: 1,
       pin: true
@@ -9520,30 +9520,30 @@ function setupDesktopAnimations() {
     y: -200,
     opacity: 1,
     scale: 1,
-    duration: 1.2
+    duration: 3.6
   }).to(".title-word", {
     y: 150,
-    duration: 1.8,
-    stagger: 1
-  }, "+=1.2").fromTo(".animate-container.integrate__pic-container .animate__column", {
+    duration: 5.4,
+    stagger: 3
+  }, "+=3.6").fromTo(".animate-container.integrate__pic-container .animate__column", {
     y: -600
   }, {
     y: 100,
     opacity: 1,
-    duration: 5.5,
+    duration: 16.5,
     ease: "none"
-  }, "-=5").fromTo(".animate-container.integrate__pic-container .animate__column", {
+  }, "-=15").fromTo(".animate-container.integrate__pic-container .animate__column", {
     rotationY: 0,
     opacity: 1,
     ease: "power2.inOut",
-    stagger: 0.2
+    stagger: 0.6
   }, {
     rotationY: 90,
     opacity: 0,
-    duration: 1.2,
+    duration: 3.6,
     ease: "power2.inOut",
-    stagger: 0.2
-  }, "-=0.5").fromTo(".animate-container.ship__pic-container .animate__column", {
+    stagger: 0.6
+  }, "-=1.5").fromTo(".animate-container.ship__pic-container .animate__column", {
     rotationY: 90,
     opacity: 0,
     y: -950
@@ -9551,28 +9551,33 @@ function setupDesktopAnimations() {
     rotationY: 0,
     opacity: 1,
     y: -950,
-    duration: 1.2,
-    stagger: 0.2,
+    duration: 3.6,
+    stagger: 0.6,
     ease: "power2.out"
-  }, "-=0.6").to(".animate-container.ship__pic-container .animate__column", {
+  }, "-=1.8").fromTo(".animate-container.ship__pic-container .animate__column", {
+    y: -950,
+    ease: "power2.out"
+  }, {
     y: -800,
-    duration: 2,
-    stagger: 0.2,
+    duration: 6,
+    stagger: 0.6,
     ease: "power2.out"
-  }, "-=1").fromTo(".ship-word", {
+  }).fromTo(".ship-word", {
     y: -1200
   }, {
     y: -800,
     ease: "power2.out",
-    duration: 1.5,
-    stagger: 0.4
-  }, "-=2.5");
+    duration: 4.7,
+    stagger: 1.2
+  }, "-=10");
+
+  //  console.log("Total duration:", tl.totalDuration());
 }
 function setupMobileAnimations() {
   const tl = gsapWithCSS.timeline({
     scrollTrigger: {
       trigger: ".animation__wrapper",
-      start: "top 0%",
+      start: "top 20%",
       scrub: 0.5,
       end: "+=800",
       pin: true,
@@ -9580,61 +9585,67 @@ function setupMobileAnimations() {
     }
   });
   tl.fromTo(".integrate__title", {
-    y: 300,
+    y: 100,
+    scale: 1.3,
     opacity: 0,
-    scale: 1.5,
     ease: "power2.out"
   }, {
-    y: 200,
+    y: 100,
+    scale: 1.3,
     opacity: 1,
+    duration: 0.1
+  }).fromTo(".integrate__title", {
+    y: 100,
+    scale: 1.3,
+    ease: "power2.out"
+  }, {
+    y: -100,
     scale: 1,
-    duration: 1.2
-  }).to(".integrate__title", {
-    y: 300,
-    opacity: 0,
-    duration: 2,
-    stagger: 1
-  }, "+=1").fromTo(".animate-container.integrate__pic-container .animate__column", {
+    duration: 2
+  }).to(".title-word", {
+    y: 200,
+    duration: 2
+  }, "+=1.5").fromTo(".animate-container.integrate__pic-container .animate__column", {
     y: -400
   }, {
-    y: 300,
+    y: 100,
     opacity: 1,
-    duration: 4,
+    duration: 5,
     ease: "none"
-  }, "-=4.5").fromTo(".animate-container.integrate__pic-container .animate__column", {
+  }, "-=5.2").fromTo(".animate-container.integrate__pic-container .animate__column", {
     rotationY: 0,
     opacity: 1,
-    ease: "power2.inOut",
-    stagger: 0.2
+    ease: "power2.inOut"
   }, {
     rotationY: 90,
     opacity: 0,
-    duration: 0.5,
+    duration: 0.6,
     ease: "power2.inOut",
     stagger: 0.2
   }, "-=0.5").fromTo(".animate-container.ship__pic-container .animate__column", {
     rotationY: 90,
     opacity: 0,
-    y: -710
+    y: -400
   }, {
     rotationY: 0,
     opacity: 1,
-    y: -710,
-    duration: 0.5,
+    y: -400,
+    duration: 0.6,
     stagger: 0.2,
     ease: "power2.out"
-  }, "-=0.6").to(".animate-container.ship__pic-container .animate__column", {
-    y: -600,
-    duration: 2,
+  }).to(".animate-container.ship__pic-container .animate__column", {
+    y: -200,
+    duration: 4,
     ease: "power2.out"
   }).fromTo(".ship__title", {
-    y: -1100
+    y: -600
   }, {
-    y: -600,
+    y: -150,
     ease: "power2.out",
     duration: 4,
     stagger: 0.2
-  }, "-=2");
+  }, "-=5");
+  console.log("Mobile duration:", tl.totalDuration());
 }
 function initAnimations() {
   if (window.innerWidth > 768) {
